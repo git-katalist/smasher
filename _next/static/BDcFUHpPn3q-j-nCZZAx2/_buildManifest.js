6b19c3614a4fe1d9a183a07117dd26de824a3cbd
git-katalist <79943903+git-katalist@users.noreply.github.com>
2021-04-30 10:00:58 +0000

    Deploying to gh-pages from @ git-katalist/smasher@a8259fd4f7e711cba68dd2f8316857307df09588 🚀

--- _next/static/BDcFUHpPn3q-j-nCZZAx2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-026b8a8b1f9b7185b81c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d1fab6f42b09305650f9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
