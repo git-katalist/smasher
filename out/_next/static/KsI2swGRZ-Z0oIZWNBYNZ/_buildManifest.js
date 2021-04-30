2d16302325cf95ececc5385c5fa0f1d3bfe7b07d
tsots <1katalist@gmail.com>
2021-04-30 12:27:34 +0200

    testing routing

--- out/_next/static/KsI2swGRZ-Z0oIZWNBYNZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-4b9f15c4d59f805a6318.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-132f5c70a95aa360947b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
